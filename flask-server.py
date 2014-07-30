039bf6f72c2d18e14fe985efe7f600d537e373d4
amayausky <amayausky@turbine.com>
2014-07-29 20:17:50 -0400

    PUT tests pass

--- flask-server.py
+++ flask-server.py
@@ -22,13 +22,13 @@ barrel = Barrel(1, '001', '1/1/2001')
 
 @app.route('/barrels', methods=['POST'])
 def create_barrel():
-    if not request.json or not request.json.get('SerialNumber') or not request.json.get('InspectionDate'):
+    if not request.json or not request.json['SerialNumber'] or not request.json['InspectionDate']:
         return jsonify(Error='400 Some barrel information is missing.'), 400
     if request.json['SerialNumber'] == barrel.serial_number:
         return jsonify(
             Error='409 A barrel with the serial number %s is already in the warehouse.' % barrel.serial_number), 409
     serial_number = request.json['SerialNumber']
-    inspection_date = request.json('InspectionDate')
+    inspection_date = request.json['InspectionDate']
     new_barrel = Barrel(3, serial_number, inspection_date)
     resp = make_response(jsonify(new_barrel.to_dict()), 201)
     resp.headers['Location'] = url_for('get_barrel', barrel_id=3)
@@ -50,6 +50,18 @@ def get_barrel(barrel_id):
         resp.headers['Content-Type'] = 'application/xml'
         return resp
     return jsonify(barrel.to_dict())
+
+
+@app.route("/barrels/<int:barrel_id>", methods=['PUT'])
+def update_barrel(barrel_id):
+    if barrel.id != barrel_id:
+        return jsonify(Error='404 No barrel with ID %s exists.' % barrel_id), 404
+    if not request.json or not request.json['SerialNumber'] and not request.json['InspectionDate']:
+        return jsonify(Error='400 Some barrel information is missing.'), 400
+    serial_number = request.json['SerialNumber'] if request.json['SerialNumber'] else barrel.serial_number
+    inspection_date = request.json['InspectionDate'] if request.json['InspectionDate'] else barrel.inspection_date
+    updated_barrel = Barrel(barrel.id, serial_number, inspection_date)
+    return make_response(jsonify(updated_barrel.to_dict()))
 
 
 if __name__ == "__main__":
